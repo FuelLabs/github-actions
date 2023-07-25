73a88104dd4b4a0a008ac47f2ed287e19c8e1026
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2023-07-25 12:40:17 -0600

    add docs checks

---
+++ docs/mdbook-docs.js
@@ -0,0 +1,136 @@
+const fs = require("fs");
+const path = require("path");
+const { EOL } = require("os");
+const assert = require("assert");
+
+const srcFolderPath = path.join(process.cwd(), process.argv[0]);
+const subfolders = fs
+  .readdirSync(srcFolderPath)
+  .filter((item) => fs.statSync(path.join(srcFolderPath, item)).isDirectory());
+const summaryFilePath = path.join(srcFolderPath, "SUMMARY.md");
+const summaryContent = fs.readFileSync(summaryFilePath, "utf-8");
+const splitSummary = summaryContent.split(EOL);
+
+function main() {
+  checkForIndexFile();
+  checkForNestedFolders();
+  checkForNestedSummaryFolders();
+  checkForUnusedFiles();
+  checkSummaryFile();
+}
+
+main();
+
+function checkForIndexFile() {
+  const indexPath = path.join(srcFolderPath, "index.md");
+  assert(fs.existsSync(indexPath));
+}
+
+function checkForNestedFolders() {
+  const nestedSubfolders = subfolders.filter((subfolder) =>
+    fs
+      .readdirSync(path.join(srcFolderPath, subfolder))
+      .some((item) =>
+        fs.statSync(path.join(srcFolderPath, subfolder, item)).isDirectory()
+      )
+  );
+  assert.deepStrictEqual(nestedSubfolders.length, 0);
+}
+function checkForNestedSummaryFolders() {
+  const nestedSubfolders = splitSummary.filter((line) =>
+    line.startsWith("    -")
+  );
+  assert.deepStrictEqual(nestedSubfolders.length, 0);
+}
+
+function checkForUnusedFiles() {
+  const fileNames = fs.readdirSync(srcFolderPath);
+  fileNames.forEach((file) => {
+    // check if each file can be found in the SUMMARY
+    if (file !== "SUMMARY.md") {
+      assert(summaryContent.includes(file));
+    }
+  });
+  subfolders.forEach((folder) => {
+    const folderPath = path.join(srcFolderPath, folder);
+    const subfolderNames = fs.readdirSync(folderPath);
+    subfolderNames.forEach((subFile) => {
+      assert(summaryContent.includes(subFile));
+    });
+  });
+}
+
+function checkSummaryFile() {
+  const order = processSummary(splitSummary);
+
+  Object.keys(order).forEach((key) => {
+    const menuOrder = order[key];
+    if (key === "menu") {
+      // check if each line in the menu corresponds to
+      // an existing top-level file or the name of the folder
+      menuOrder.forEach((item) => {
+        let itemPath = path.join(srcFolderPath, item);
+        if (fs.existsSync(itemPath)) {
+          assert(fs.statSync(itemPath).isDirectory());
+        } else {
+          itemPath = `${itemPath}.md`;
+          assert(fs.existsSync(itemPath));
+        }
+      });
+    } else {
+      // check if item exists in the right folder
+      const possibleSeparators = ["-", "_"];
+      menuOrder.forEach((item) => {
+        let fileExists = false;
+        for (const separator of possibleSeparators) {
+          const newItem = item.replace(/[-_]/g, separator);
+          let itemPath = path.join(srcFolderPath, `${key}/${newItem}.md`);
+          if (fs.existsSync(itemPath)) {
+            fileExists = true;
+            break;
+          }
+        }
+        assert(fileExists);
+      });
+    }
+  });
+}
+
+function processSummary(lines) {
+  const order = { menu: [] };
+  let currentCategory;
+  lines.forEach((line) => {
+    const paths = line.split("/");
+    const newPaths = paths[0].split("(");
+    const thisCat = currentCategory;
+    if (line.includes(".md")) {
+      if (line[0] === "-") {
+        // handle top-level items
+        if (paths.length > 2) {
+          currentCategory = paths[paths.length - 2];
+        } else if (
+          paths[paths.length - 1].includes("index.md") ||
+          newPaths[newPaths.length - 1].endsWith(".md)")
+        ) {
+          currentCategory = newPaths[newPaths.length - 1];
+        } else {
+          currentCategory = paths[paths.length - 1];
+        }
+        const final = currentCategory.replace(".md)", "");
+        if (thisCat === final) {
+          const fileName = paths[paths.length - 1].replace(".md)", "");
+          if (!order[currentCategory]) order[currentCategory] = [];
+          order[currentCategory].push(fileName);
+        } else if (final !== "index") {
+          order.menu.push(final);
+        }
+      } else if (currentCategory) {
+        // handle sub-paths
+        const fileName = paths[paths.length - 1].replace(".md)", "");
+        if (!order[currentCategory]) order[currentCategory] = [];
+        if (fileName !== "index") order[currentCategory].push(fileName);
+      }
+    }
+  });
+  return order;
+}
---
+++ docs/next-docs.mjs
@@ -0,0 +1,128 @@
+import path from "path";
+import fs from "fs";
+import assert from "assert";
+import remarkMdx from "remark-mdx";
+import remarkParse from "remark-parse";
+import { unified } from "unified";
+import { visit } from "unist-util-visit";
+
+const DOCS_DIRECTORY = path.join(process.cwd(), `../../${process.argv[2]}`);
+const SRC_PATH = path.join(process.cwd(), `../../${process.argv[3]}`);
+const COMP_CONFIG_PATH = path.join(SRC_PATH, "components.json");
+const NAV_PATH = path.join(SRC_PATH, "nav.json");
+const files = fs.readdirSync(DOCS_DIRECTORY);
+const subfolders = files.filter((item) =>
+  fs.statSync(path.join(DOCS_DIRECTORY, item)).isDirectory()
+);
+
+function main(){
+  checkForNestedFolders();
+  checkNavConfig();
+  checkComponentsConfig();
+  checkComponentNames();
+  checkComponentNesting();
+}
+
+main();
+
+function checkForNestedFolders(){
+  const nestedSubfolders = subfolders.filter((subfolder) =>
+      fs
+        .readdirSync(path.join(DOCS_DIRECTORY, subfolder))
+        .some((item) =>
+          fs.statSync(path.join(DOCS_DIRECTORY, subfolder, item)).isDirectory()
+        )
+    );
+    assert.deepStrictEqual(nestedSubfolders.length, 0);
+}
+
+function checkNavConfig(){
+  const navFile = fs.readFileSync(NAV_PATH, "utf8");
+  const navJSON = JSON.parse(navFile);
+  assert(Array.isArray(navJSON.menu));
+  subfolders.forEach((folder) => {
+    assert(Array.isArray(navJSON[folder.replaceAll("-", "_")]));
+  });
+}
+
+function checkComponentsConfig(){
+  const compFile = fs.readFileSync(COMP_CONFIG_PATH, "utf8");
+  const compJSON = JSON.parse(compFile);
+  assert(Array.isArray(compJSON.folders));
+  assert(Array.isArray(compJSON.ignore));
+}
+
+function checkComponentNames(){
+  files.forEach((filename) => {
+    const filepath = path.join(DOCS_DIRECTORY, filename);
+    if (fs.statSync(filepath).isDirectory()) {
+      const subFiles = fs.readdirSync(filepath);
+      subFiles.forEach((subFilename) => {
+        const subFilepath = path.join(filepath, subFilename);
+        checkFile(subFilepath);
+      });
+    } else {
+      checkFile(filepath);
+    }
+  });
+}
+
+// Examples.Events.Connect && Examples.Connect is ok
+// Examples.Events.Connect.First is not ok
+function checkComponentNesting(){
+  let allComponents = [];
+  files.forEach((filename) => {
+    const filepath = path.join(DOCS_DIRECTORY, filename);
+    if (fs.statSync(filepath).isDirectory()) {
+      const subFiles = fs.readdirSync(filepath);
+      subFiles.forEach((subFilename) => {
+        const subFilepath = path.join(filepath, subFilename);
+        const file = fs.readFileSync(subFilepath, "utf8");
+        const components = getComponents(file);
+        allComponents = [...allComponents, ...components];
+      });
+    } else {
+      const file = fs.readFileSync(filepath, "utf8");
+      const components = getComponents(file);
+      allComponents = [...allComponents, ...components];
+    }
+    const cleaned = Array.from(new Set(allComponents));
+    cleaned.forEach((compName) => {
+      const length = compName.split(".").length;
+      assert(length < 4);
+    });
+  });
+}
+
+function checkFile(filepath) {
+  const file = fs.readFileSync(filepath, "utf8");
+  const components = getComponents(file);
+  const compFile = fs.readFileSync(COMP_CONFIG_PATH, "utf8");
+  const compJSON = JSON.parse(compFile);
+  components.forEach((comp) => {
+    if (!compJSON.ignore.includes(comp)) {
+      let actualCompPath = "";
+      for (let i = 0; i < compJSON.folders.length; i++) {
+        const path = `${compJSON.folders[i]}/${
+          comp.includes(".") ? comp.split(".").pop() : comp
+        }`;
+        const actualPath = `${process.cwd()}${path}.tsx`;
+        if (fs.existsSync(actualPath)) {
+          actualCompPath = `..${path}`;
+          break;
+        }
+      }
+      assert.notDeepStrictEqual(actualCompPath, "");
+    }
+  });
+}
+
+function getComponents(mdxContent) {
+  const components = [];
+  const tree = unified().use(remarkParse).use(remarkMdx).parse(mdxContent);
+
+  visit(tree, "mdxJsxFlowElement", (node) => {
+    if (node.name) components.push(node.name);
+  });
+  return Array.from(new Set(components));
+}
