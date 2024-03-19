6a19c1f4129cc5d5499f38775bfcf7f7d9260283
Hélcio Franco <helciofranco@me.com>
2024-03-19 18:19:39 -0400

    feat: add support to packages comma-separated

--- update-sdk/src/index.ts
+++ update-sdk/src/index.ts
@@ -5,7 +5,8 @@ async function main() {
   const repository = core.getInput('repository');
   const branch = core.getInput('branch');
   const npmTag = core.getInput('npm-tag');
-  const packages = core.getMultilineInput('packages');
+  const pkgInput = core.getInput('packages');
+  const packages = pkgInput.includes(',') ? pkgInput.split(',') : core.getMultilineInput('packages');
 
   const bot = new ReleaseBot(repository, branch, npmTag, packages);
   await bot.release();
