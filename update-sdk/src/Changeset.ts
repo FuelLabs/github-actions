f9cbf264cdd19be8b80ad82053cbc753c0d889d1
Hélcio Franco <helciofranco@me.com>
2024-03-20 16:32:04 -0400

    feat: convert it to promise

--- update-sdk/src/Changeset.ts
+++ update-sdk/src/Changeset.ts
@@ -9,45 +9,56 @@ export class Changeset {
     console.log('📝 Adding changeset for packages\n');
     console.log('📟 pnpm changeset add\n');
 
-    const childProcess = spawn('pnpm', ['changeset', 'add']);
-    let hasSelectedPackages = false;
-    let hasSkippedMajor = false;
-    let hasSkippedMinor = false;
-    let hasSkippedSummary = false;
-
     // Manually fill the changeset interactive CLI
-    childProcess.stdout.on('data', (data) => {
-      const text = data.toString();
-      const changed = text.includes('changed packages');
-      if (changed && !hasSelectedPackages) {
-        childProcess.stdin.write(' ');
-        childProcess.stdin.write('\n');
-        hasSelectedPackages = true;
-      }
+    return new Promise((resolve, reject) => {
+      const childProcess = spawn('pnpm', ['changeset', 'add']);
+      let hasSelectedPackages = false;
+      let hasSkippedMajor = false;
+      let hasSkippedMinor = false;
+      let hasSkippedSummary = false;
 
-      const major = text.includes('major bump');
-      if (major && !hasSkippedMajor) {
-        childProcess.stdin.write('\n');
-        hasSkippedMajor = true;
-      }
+      // Manually fill the changeset interactive CLI
+      childProcess.stdout.on('data', (data) => {
+        const text = data.toString();
+        const changed = text.includes('changed packages');
+        if (changed && !hasSelectedPackages) {
+          childProcess.stdin.write(' ');
+          childProcess.stdin.write('\n');
+          hasSelectedPackages = true;
+        }
 
-      const minor = text.includes('minor bump');
-      if (minor && !hasSkippedMinor) {
-        childProcess.stdin.write('\n');
-        hasSkippedMinor = true;
-      }
+        const major = text.includes('major bump');
+        if (major && !hasSkippedMajor) {
+          childProcess.stdin.write('\n');
+          hasSkippedMajor = true;
+        }
 
-      const summary = text.includes('summary for this change');
-      if (summary && !hasSkippedSummary) {
-        childProcess.stdin.write(`ci: update to tag ${npmTag}\n`);
-        childProcess.stdin.write('\n');
-        hasSkippedSummary = true;
-      }
+        const minor = text.includes('minor bump');
+        if (minor && !hasSkippedMinor) {
+          childProcess.stdin.write('\n');
+          hasSkippedMinor = true;
+        }
 
-      const finish = text.includes('desired changeset');
-      if (finish) {
-        childProcess.stdin.write('\n');
-      }
+        const summary = text.includes('summary for this change');
+        if (summary && !hasSkippedSummary) {
+          childProcess.stdin.write(`ci: update to tag ${npmTag}\n`);
+          childProcess.stdin.write('\n');
+          hasSkippedSummary = true;
+        }
+
+        const finish = text.includes('desired changeset');
+        if (finish) {
+          childProcess.stdin.write('\n');
+        }
+      });
+
+      childProcess.on('close', (code) => {
+        if (code === 0) {
+          resolve(true);
+        } else {
+          reject(new Error(`Child process exited with code ${code}`));
+        }
+      });
     });
   }
 }
